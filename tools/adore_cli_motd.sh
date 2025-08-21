8b0c947892447caf73b7d3c3d02c2fef11387c91
Andrew Koerner <andrew.koerner@dlr.de>
2025-08-21 11:21:02 +0200

    updated the message of the day to be more... fun
    Signed-off-by: Andrew Koerner <andrew.koerner@dlr.de>

--- tools/adore_cli_motd.sh
+++ tools/adore_cli_motd.sh
@@ -2,10 +2,12 @@
 
 printf "Welcome to the ADORe Development CLI %s (%s %s %s)\n\n" "$(lsb_release -a 2>/dev/null | grep Description | cut -d: -f2 | xargs)" "$(uname -o)" "$(uname -r)" "$(uname -m)"
 
-printf "            ____                       _    ____   ___  ____      \n"
-printf "         __/  |_\__                   / \  |  _ \ / _ \|  _ \ ___ \n"
-printf '        |           -.               / _ \ | | | | | | | |_) / _ \\\n'
-printf "  ......'-(_)---(_)--'              / ___ \| |_| | |_| |  _ <  __/\n"
-printf "                                    \_/  \_\____/ \___/|_| \_\___|\n"
+printf "    _    ____   ___  ____                              \n"
+printf "   / \  |  _ \ / _ \|  _ \ ___            ____        \n"
+printf "  / _ \ | | | | | | | |_) / _ \        __/  |_\__     \n"
+printf " / ___ \| |_| | |_| |  _ <  __/       |           -.  \n"
+printf " \_/  \_\____/ \___/|_| \_\___| ......'-(_)---(_)--'  \n"
+
+
 printf "\n"
 printf "  Type 'help' for more information.\n"
